4f68b0fac5908eba7ce8f0dfc5e8c0c191e0bcb2
Arsen4ik <arsenfazleev@gmail.com>
2022-12-03 23:48:29 +0300

    что

--- script.js
+++ script.js
@@ -2,25 +2,30 @@ const app = Vue.createApp({
     data(){
         return {
             date: new Date().getFullYear(),
-            // pict1: true,
-            // pict2: true,
-            // pict3: true,
-            // pict4: true,
+            // connect: true
         }
     },
-    // methods: {
-    //     changePict1(){
-    //         this.pict1 = !this.pict1
-    //     }, 
-    //     changePict2(){
-    //         this.pict2 = !this.pict2
-    //     },
-    //     changePict3(){
-    //         this.pict3 = !this.pict3
-    //     },
-    //     changePict4(){
-    //         this.pict4 = !this.pict4
-    //     }
-    // }
+    computed: {
+        // connects(){
+        //     return this.connect == false
+        // }
+    },
+    methods: {
+        // changePict1(){
+        //     this.pict1 = !this.pict1
+        // }, 
+        // changePict2(){
+        //     this.pict2 = !this.pict2
+        // },
+        // changePict3(){
+        //     this.pict3 = !this.pict3
+        // },
+        // changePict4(){
+        //     this.pict4 = !this.pict4
+        // }
+        // connection(){
+        //     this.connect = !this.connect
+        // }
+    }
 })
 app.mount('body')
