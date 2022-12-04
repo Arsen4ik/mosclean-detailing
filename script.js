02d030f851fceb9cc46047d3b9dcbf4bd7fd290c
Arsen4ik <arsenfazleev@gmail.com>
2022-12-04 17:04:43 +0300

    добавлены connections

--- script.js
+++ script.js
@@ -5,26 +5,10 @@ const app = Vue.createApp({
             // connect: true
         }
     },
-    computed: {
-        // connects(){
-        //     return this.connect == false
-        // }
-    },
     methods: {
-        // changePict1(){
-        //     this.pict1 = !this.pict1
-        // }, 
-        // changePict2(){
-        //     this.pict2 = !this.pict2
-        // },
-        // changePict3(){
-        //     this.pict3 = !this.pict3
-        // },
-        // changePict4(){
-        //     this.pict4 = !this.pict4
-        // }
         // connection(){
         //     this.connect = !this.connect
+        //     console.log('gg')
         // }
     }
 })
