e223836d7cd0f206d3b128386609ae2fca8835ae
Arsen4ik <arsenfazleev@gmail.com>
2022-12-11 12:08:10 +0300

    еще немного

---
+++ dist/index.672d4772.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data () {
+        return {
+            date: new Date().getFullYear()
+        };
+    },
+    methods: {
+    }
+});
+app.mount("body");
+
+//# sourceMappingURL=index.672d4772.js.map
